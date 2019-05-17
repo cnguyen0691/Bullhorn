87368fb599877e4d6dbfe2530c6c06d53f818c02
GBTC440009UR <chaunguyen0691@gmail.com>
2019-05-17 13:19:53 -0400

    add image

--- src/main/java/com/example/bullhorn/HomeController.java
+++ src/main/java/com/example/bullhorn/HomeController.java
@@ -4,12 +4,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.validation.Valid;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
 
 
 @Controller
@@ -22,6 +23,14 @@ public class HomeController {
         model.addAttribute("messages", messageRepository.findAll());
         return "list";
     }
+//    @RequestMapping(value = "{image}")
+//    @ResponseBody
+//    public byte[] getImage(@PathVariable(value = "image") String imageName) throws IOException {
+//
+//        File serverFile = new File("/home/user/uploads/" + imageName + ".jpg");
+//
+//        return Files.readAllBytes(serverFile.toPath());
+//    }
 
     @GetMapping("/add")
     public String addmessage(Model model){
@@ -36,7 +45,9 @@ public class HomeController {
         }
         messageRepository.save(message);
         return ("redirect:/");
+
     }
+
     @RequestMapping("/update/{id}")
     public String updateMessage(@PathVariable("id") long id, Model model) {
         model.addAttribute("message", messageRepository.findById(id).get());
